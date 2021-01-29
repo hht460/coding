b26d940793b6af144da9df7aab049768a68024dc
hthu <18252093401@163.com>
2021-01-29 17:41:51 +0800

    11

--- guide/src/com/hthu/java/thread/alternateprint/AlternatePrint.java
+++ guide/src/com/hthu/java/thread/alternateprint/AlternatePrint.java
@@ -1,5 +1,8 @@
 package thread.alternateprint;
 
+/**
+ * 两个线程交替打印
+ */
 public class AlternatePrint {
     public static void main(String[] args) {
         ThreadOne threadOne = new ThreadOne("奇数线程");
