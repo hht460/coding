326cf5007e7dc374f988f56cfa740bd1f1c571f2
hthu <18252093401@163.com>
2021-02-01 17:53:10 +0800

    aa

---
+++ guide/src/main/java/org/hthu/tree/BinarySearchTree.java
@@ -0,0 +1,30 @@
+package org.hthu.tree;
+
+/**
+ * 二叉搜索树
+ */
+public class BinarySearchTree {
+
+    private Node node;
+
+    /**
+     * 二叉搜索树查找操作
+     * @param data
+     * @return
+     */
+    public Node find(int data){
+    }
+
+    /**
+     * 静态内部类,构造节点数据结构
+     */
+    public static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
