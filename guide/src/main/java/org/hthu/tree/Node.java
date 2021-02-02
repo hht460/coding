5747d7c3f04268b023351cbcdf3ddedd0fdc4a59
hthu <18252093401@163.com>
2021-02-02 23:03:12 +0800

    二叉树遍历

---
+++ guide/src/main/java/org/hthu/tree/Node.java
@@ -0,0 +1,15 @@
+package org.hthu.tree;
+
+/**
+ * 构造节点数据结构
+ */
+public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+}
---
+++ guide/src/main/java/org/hthu/tree/PrintTree.java
@@ -0,0 +1,88 @@
+package org.hthu.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 遍历二叉树
+ */
+public class PrintTree {
+
+    /**
+     * 前序遍历二叉树 节点-->左子树-->右子树
+     * @param node
+     */
+    public void preOrder(Node node){
+        // 截至条件
+        if(node == null){
+            return;
+        }
+        // 打印当前节点
+        System.out.println(node.data);
+        // 递归左子树
+        preOrder(node.left);
+        // 递归右子树
+        preOrder(node.right);
+    }
+
+    /**
+     * 中序遍历 左子树-->节点-->右子树
+     * @param node
+     */
+    public void inOrder(Node node){
+        // 截至条件
+        if (node == null){
+            return;
+        }
+        // 递归左子树
+        preOrder(node.left);
+        // 打印当前节点
+        System.out.println(node.data);
+        // 递归右子树
+        preOrder(node.right);
+    }
+
+    /**
+     * 后续遍历, 左子树-->右子树-->节点
+     * @param node
+     */
+    public void posOrder(Node node){
+        // 截至条件
+        if (node == null){
+            return;
+        }
+        // 递归左子树
+        preOrder(node.left);
+        // 递归右子树
+        preOrder(node.right);
+        // 打印当前节点
+        System.out.println(node.data);
+    }
+
+    /**
+     * 二叉树层序编列,利用队列辅助打印
+     * @param node
+     */
+    public void levelOrder(Node node){
+        if (node == null){
+            return;
+        }
+        // 定义一个队列
+        Queue<Node> queue = new LinkedList<>();
+        // 将二叉树根节点poll进队列
+        queue.offer(node);
+        while (!queue.isEmpty()){
+            // 从队列弹出一个根节点
+            Node curNode = queue.poll();
+            System.out.println(curNode.data);
+            // 当前节点左子树不为空,放入队列中
+            if (curNode.left !=null){
+                queue.offer(curNode.left);
+            }
+            // 当前节点右子树不为空,放入队列中
+            if (curNode.right !=null){
+                queue.offer(curNode.right);
+            }
+        }
+    }
+}
