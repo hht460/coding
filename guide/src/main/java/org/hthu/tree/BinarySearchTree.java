e081c6e5cdd1f864178ed36a411b36fa170c0a12
hthu <18252093401@163.com>
2021-02-02 17:33:38 +0800

    link insert

--- guide/src/main/java/org/hthu/tree/BinarySearchTree.java
+++ guide/src/main/java/org/hthu/tree/BinarySearchTree.java
@@ -1,10 +1,11 @@
 package org.hthu.tree;
 
 /**
- * 二叉搜索树,几种基本操作
+ * 二叉搜索树(BST),几种基本操作
  */
 public class BinarySearchTree {
 
+    // 持有二叉搜索树指针
     private Node node;
 
     /**
@@ -13,18 +14,55 @@ public class BinarySearchTree {
      * @return
      */
     public Node find(int data){
-        while(node !=null){
+        Node tmp = node;
+        while(tmp !=null){
             // 大于当前节点值,遍历右子树
-            if (node.data < data){
-                node = node.right;
+            if (tmp.data < data){
+                tmp = tmp.right;
             }
             // 小于当前节点值,遍历左子树
-            if(node.data > data){
-                node = node.left;
+            else if(tmp.data > data){
+                tmp = tmp.left;
+            }else {
+                return tmp;
             }
-            return node;
         }
         return null;
+    }
+
+    /**
+     * 二叉搜索树插入操作
+     * @param data
+     */
+    public void insert(int data){
+        // 空二叉搜索树
+        if (node == null){
+            node = new Node(data);
+            return;
+        }
+        //
+        while (node !=null){
+            // 比当前节点数据小,遍历左子树
+            if (node.data > data){
+                // 左子树为空
+                if (node.left == null){
+                    node.left = new Node(data);
+                    return;
+                }
+                // 左子树不为空,继续递归遍历左子树
+                node = node.left;
+            }
+            // 比当前节点数据大,遍历右子树
+            if (node.data < data){
+                // 右子树为空
+                if (node.right == null){
+                    node.right = new Node(data);
+                    return;
+                }
+                // 右子树不为空,继续递归遍历右子树
+                node = node.right;
+            }
+        }
     }
 
     /**
