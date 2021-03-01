90e814482193763eb3e6c65a146ced7465512a90
hthu <18252093401@163.com>
2021-03-01 15:06:07 +0800

    update BubbleSort

--- guide/src/main/java/org/hthu/sort/BubbleSort.java
+++ guide/src/main/java/org/hthu/sort/BubbleSort.java
@@ -8,25 +8,49 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] a = new int[]{1, 4, 12, 23};
-        sort(a);
+        optimizeSort(a);
         System.out.println(Arrays.toString(a));
     }
 
     // 改1
-    public static void sort(int[] a){
-        boolean flag = false;
-        for (int i=0;i<a.length;i++){
-            for (int j=0;j<a.length-i-1;j++){
-                if (a[j]>a[j+1]){
+    public static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
                     flag = true;
-                    int temp = a[j+1];
-                    a[j+1] = a[j];
+                    int temp = a[j + 1];
+                    a[j + 1] = a[j];
                     a[j] = temp;
                 }
             }
-            if (flag){
+            if (flag) {
                 break;
             }
         }
     }
+
+    // 改2,增加比较边界,和是否继续比较标志
+    public static void optimizeSort(int[] a) {
+        int temp = 0;
+        int sortBorder = a.length - 1;
+        int lastIndex = 0;
+        for (int i = 0; i < a.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < sortBorder; j++) {
+                if (a[j] > a[j + 1]) {
+                    flag = true;
+                    temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+                    lastIndex = j; // 记录下最后一次交换元素位置,下次交换仅仅比较到这里即停止
+                }
+            }
+            sortBorder = lastIndex;
+            if (flag) {
+                break;
+            }
+        }
+    }
+
 }
