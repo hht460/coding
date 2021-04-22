b8f99f4efc0e5b86dacf554d7f8f5554865a47bb
hthu <18252093401@163.com>
2021-04-23 07:51:22 +0800

    uu

--- guide/src/main/java/org/hthu/jzoffer/JZ22.java
+++ guide/src/main/java/org/hthu/jzoffer/JZ22.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。层序打印二叉树
  */
 public class JZ22 {
     public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
