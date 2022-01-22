87f65ff6dc5b5b9b103427f68acbf8008d018164
haitao.hu2 <haitao.hu2@amh-group.com>
2022-01-22 11:06:30 +0800

    x

--- guide/src/main/java/org/hthu/test/Main.java
+++ guide/src/main/java/org/hthu/test/Main.java
@@ -3,6 +3,7 @@ package org.hthu.test;
 public class Main {
     public static void main(String[] args) {
         MasStringTool masStringTool = Sinleton.getMasStringTool();
+
         System.out.println(masStringTool.maxStr("ababab"));
     }
 }
