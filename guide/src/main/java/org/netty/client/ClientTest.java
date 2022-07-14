0ffad4f155bc665af84b56067336047765b1be25
haitao.hu2 <haitao.hu2@amh-group.com>
2022-07-14 08:22:07 +0800

    add netty

---
+++ guide/src/main/java/org/netty/client/ClientTest.java
@@ -0,0 +1,9 @@
+package org.netty.client;
+
+/**
+ * @author haitao.hu2
+ * @version 0.0.1
+ * @date 2022/7/14
+ */
+public class ClientTest {
+}
---
+++ guide/src/main/java/org/netty/server/ServiceTest.java
@@ -0,0 +1,9 @@
+package org.netty.server;
+
+/**
+ * @author haitao.hu2
+ * @version 0.0.1
+ * @date 2022/7/14
+ */
+public class ServiceTest {
+}
